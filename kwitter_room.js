d3481c179bb2fcc2c0e208e0f99b923ba8c1d491
bhavya1254 <114231177+bhavya1254@users.noreply.github.com>
2023-01-06 18:55:46 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,37 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyD_GAUJAU-2VzViZXjraX4pH99m6uC8znM",
+      authDomain: "kwitter-90da2.firebaseapp.com",
+      databaseURL: "https://kwitter-90da2-default-rtdb.firebaseio.com",
+      projectId: "kwitter-90da2",
+      storageBucket: "kwitter-90da2.appspot.com",
+      messagingSenderId: "243618268507",
+      appId: "1:243618268507:web:597cda0fcd7ab3c1a7867e"
+    };
+    firebase.initializeApp(firebaseConfig);  
+
+function addRoom()
+{
+      user_name = document.getElementById("user_name").value;
+      firebase.database().ref("/").child(user_name).update({
+            purpose : "adding user"
+      });
+}
+
+
+
+
+
+
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
